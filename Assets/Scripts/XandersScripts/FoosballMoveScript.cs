a3f27a7b5e4c75b5f286dc6ef1907360cc623c0c
Xd0uc3tt3 <145155890+Xd0uc3tt3@users.noreply.github.com>
2025-09-22 17:24:06 -0300

    Finished Game
    Made moving players in Level Three "FoosBall"

---
+++ Assets/Scripts/XandersScripts/FoosballMoveScript.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    [SerializeField]
+    private WaypointPath _waypointPath;
+
+    [SerializeField]
+    private float _speed;
+
+    private int _targetWaypointIndex;
+
+    private Transform _previousWaypoint;
+    private Transform _targetWaypoint;
+
+    private float _timeToWaypoint;
+    private float _elapsedTime;
+
+    void Start()
+    {
+        TargetNextWaypoint();
+    }
+
+    void Update()
+    {
+        _elapsedTime += Time.deltaTime;
+
+        float elapsedPercentage = _elapsedTime / _timeToWaypoint;
+        transform.position = Vector3.Lerp(_previousWaypoint.position, _targetWaypoint.position, elapsedPercentage);
+
+        if (elapsedPercentage >= 1f)
+        {
+            TargetNextWaypoint();
+        }
+    }
+
+    private void TargetNextWaypoint()
+    {
+        _previousWaypoint = _waypointPath.GetWaypoint(_targetWaypointIndex);
+        _targetWaypointIndex = _waypointPath.GetNextWaypointIndex(_targetWaypointIndex);
+        _targetWaypoint = _waypointPath.GetWaypoint(_targetWaypointIndex);
+
+        _elapsedTime = 0f;
+
+        float distanceToWaypoint = Vector3.Distance(_previousWaypoint.position, _targetWaypoint.position);
+        _timeToWaypoint = distanceToWaypoint / _speed;
+    }
+}
---
+++ Assets/Scripts/XandersScripts/Waypoint.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class WaypointPath : MonoBehaviour
+{
+    public Transform GetWaypoint(int waypointIndex)
+    {
+        return transform.GetChild(waypointIndex);
+    }
+
+    public int GetNextWaypointIndex(int currentWaypointIndex)
+    {
+        int nextWaypointIndex = currentWaypointIndex + 1;
+
+        if (nextWaypointIndex >= transform.childCount)
+        {
+            nextWaypointIndex = 0; // loop back to start
+        }
+
+        return nextWaypointIndex;
+    }
+}
+
